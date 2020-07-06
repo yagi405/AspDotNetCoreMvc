9b1c03a47ca3ef657ddec363bb3a579d27118166
Hiroki Saito <gregg490206@gmail.com>
2020-07-06 15:22:00 +0900

    add CustomAttribute project

---
+++ src/AspDotNetCoreMvc/CustomAttribute/Models/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using CustomAttribute.Models.Extensions;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+
+namespace CustomAttribute.Models.Attributes
+{
+    /// <summary>
+    /// Ajax呼び出しであるかを検証するアクションセレクターです。
+    /// </summary>
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            return routeContext.HttpContext.Request.IsAjaxRequest();
+        }
+    }
+}
---
+++ src/AspDotNetCoreMvc/CustomAttribute/Models/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace CustomAttribute.Models.Extensions
+{
+    /// <summary>
+    /// <see cref="HttpRequest"/> クラスの拡張メソッドを提供するクラスです。
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+        private const string AjaxRequestHeaderItem = "X-Requested-With";
+        private const string AjaxRequestHeaderValue = "XMLHttpRequest";
+
+        /// <summary>
+        /// Ajaxによるリクエストかどうかを取得します。
+        /// </summary>
+        /// <param name="self"><see cref="HttpRequest"/></param>
+        /// <returns>Ajaxによるリクエストである場合は true それ以外は false</returns>
+        public static bool IsAjaxRequest(this HttpRequest self)
+        {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            return self.Headers != null &&
+                   self.Headers[AjaxRequestHeaderItem] == AjaxRequestHeaderValue;
+        }
+    }
+
+}
