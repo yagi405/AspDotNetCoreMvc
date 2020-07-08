49ed451d76fd9d06803772bd8f0384b4d87568da
Hiroki Saito <gregg490206@gmail.com>
2020-07-08 13:06:44 +0900

    add ChatMapper

---
+++ src/ChatApp/ChatApp/Models/Mappers/IChatMapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ChatApp.Models.Entities.DbEntities;
+using ChatApp.Models.Entities.ViewEntities;
+
+namespace ChatApp.Models.Mappers
+{
+    public interface IChatMapper
+    {
+        IList<ChatLogViewModel> FromChatLogToViewModel(IList<ChatLog> chatLogs);
+    }
+}
---
+++ src/ChatApp/ChatApp/Models/Mappers/Imp/ChatMapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ChatApp.Models.Entities.DbEntities;
+using ChatApp.Models.Entities.ViewEntities;
+
+namespace ChatApp.Models.Mappers.Imp
+{
+    public class ChatMapper : IChatMapper
+    {
+        public IList<ChatLogViewModel> FromChatLogToViewModel(IList<ChatLog> chatLogs)
+        {
+            return chatLogs
+                .Select(x => new ChatLogViewModel()
+                {
+                    PostAt = x.PostAt,
+                    Message = x.Message,
+                })
+                .ToList();
+        }
+    }
+}
