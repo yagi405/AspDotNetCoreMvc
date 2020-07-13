9d36e22ab32a15fe594e2cff511ee127ba863c2b
Hiroki Saito <gregg490206@gmail.com>
2020-07-13 09:22:56 +0900

    enable nlog

--- src/ChatApp/ChatApp/Program.cs
+++ src/ChatApp/ChatApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace ChatApp
 {
@@ -21,6 +22,12 @@ namespace ChatApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
