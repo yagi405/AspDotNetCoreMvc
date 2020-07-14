dcaed7653e325ca280fe789f52a5b62c659cd9e4
Hiroki Saito <gregg490206@gmail.com>
2020-07-14 22:00:28 +0900

    add HelloWorldWithModel project

---
+++ src/AspDotNetCoreMvc/HelloWorldWithModel/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorldWithModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWithModel.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult List()
+        {
+            var persons = new List<Person>()
+            {
+                new Person("山田", 41, new DateTime(1976, 5, 5), false),
+                new Person("齋藤", 38, new DateTime(1980, 7, 18), false),
+                new Person("田中", 15, new DateTime(2005, 10, 3), false),
+                new Person("高橋", 24, new DateTime(1998, 2, 6), true),
+            };
+
+            return View(persons);
+        }
+    }
+}
---
+++ src/AspDotNetCoreMvc/HelloWorldWithModel/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldWithModel.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDay { get; set; }
+        public bool IsMarried { get; set; }
+
+        public Person(string name, int age, DateTime birthDay, bool isMarried) =>
+            (Name, Age, BirthDay, IsMarried) = (name, age, birthDay, isMarried);
+    }
+}
