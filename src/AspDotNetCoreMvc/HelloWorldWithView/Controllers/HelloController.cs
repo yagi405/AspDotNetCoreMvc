78995ffe6458a899568c2e06ef5fb2aae27849f2
Hiroki Saito <gregg490206@gmail.com>
2020-07-14 16:42:45 +0900

    add HelloWorldWithView project

---
+++ src/AspDotNetCoreMvc/HelloWorldWithView/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldWithView.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            ViewData["Now"] = DateTime.Now;
+            return View();
+        }
+    }
+}
---
+++ src/AspDotNetCoreMvc/HelloWorldWithView/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorldWithView.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
