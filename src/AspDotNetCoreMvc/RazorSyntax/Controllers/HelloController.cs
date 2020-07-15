722733bab5668dec6016e9144099daf3382f03e4
Hiroki Saito <gregg490206@gmail.com>
2020-07-15 11:30:57 +0900

    add RazorSyntax project

---
+++ src/AspDotNetCoreMvc/RazorSyntax/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RazorSyntax.Models;
+
+namespace RazorSyntax.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+        public IActionResult Details()
+        {
+            var person = new Person("齋藤", 38, new DateTime(1980, 7, 18), false);
+            return View(person);
+        }
+    }
+}
---
+++ src/AspDotNetCoreMvc/RazorSyntax/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorSyntax.Models
+{
+    public class Person
+    {
+        [DisplayName("名前")]
+        public string Name { get; set; }
+
+        [DisplayName("年齢")]
+        [Url]
+        public int Age { get; set; }
+
+        [DisplayName("誕生日")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+
+        [DisplayName("既婚")]
+        public bool IsMarried { get; set; }
+
+        public Person(string name, int age, DateTime birthDay, bool isMarried) =>
+            (Name, Age, BirthDay, IsMarried) = (name, age, birthDay, isMarried);
+    }
+}
